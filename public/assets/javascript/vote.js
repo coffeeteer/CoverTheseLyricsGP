20f38452bf8c29b12eb90ee57916f93327c29439
Seth Keel <seth.keel@gmail.com>
2016-08-22 13:06:58 -0400

    trying to resolve conflict

--- public/assets/javascript/vote.js
+++ public/assets/javascript/vote.js
@@ -1,47 +1,92 @@
-$(document).ready(function(){   
+$(document).ready(function() {
 $('.votebutton').on("click", function() {
+    console.log('clicked')
 
-console.log('clicked')
-
-
- $.get('/vote', function(data) {
-     data = data[0];
-     console.log(data);
- });
-
-var entryid = $(this).attr("id");
+    var entry_id = $(this).attr("id");
     console.log($(this).attr("id"));
-    //console.log("id",id);
+
     var ip
 
-getip();
+    getIP();
 
-function getip(){
+function getIP() {
+    $(function() {
+        $.getJSON("https://api.ipify.org?format=jsonp&callback=?",
+            function(json) {
+                ip = json.ip;
+                votehandle(ip);
+            });
+    });
+}; // end ip get
 
- $(function() {
-     $.getJSON(
-         "https://api.ipify.org?format=jsonp&callback=?",
-         function(json) {
-             ip = json.ip;
-             console.log(
-                 "My public IP address is3: ",
-                 ip);
-              votehandle(ip);
-         });
-   }); // end ip get 
-   // votehandle(ip);
-};
+function votehandle(ip) {
+     console.log(ip)
+     var url = '/vote/' + ip + '/' + entry_id;
+     console.log(url);
 
-function votehandle(ip){
-    console.log(ip)
-    var url ='/vote/' + ip + '/'+ entryid;
-        console.log(url);
+     $.get(url, function(result) {
+           console.log('vote get result',result);
 
- $.post(url, function(data) {
-    data = data[0];
-    console.log(data);
-});
-}
+         if (result === null) {
+            voteNew(url);
 
-});  //end on votebutton click
-});//send on ready function
+         } else {
+            voteUpdate(url);
+        }
+             
+    function voteNew(url) {
+        console.log('true:vote_count === null')
+        var voteUrl = url + '/' + 1;
+        $.post(voteUrl, function(result) {
+            console.log(voteUrl);
+            result = result[0];
+            console.log('$.post new rec', result);
+        });  //end newVote
+    } //end newVote
+
+
+    function voteUpdate(url) {
+        console.log('in voteUpdate', result);
+        var created = result.createdAt;
+        var dateCheck = dateCalc(created);
+        var votes = result.vote_counts;
+        console.log('url',url)
+
+        if (votes <= 2 && dateCheck == true) {
+            console.log('in vote logic')
+            var votecount = votes + 1;
+            console.log('vote',votes,'votecount',votecount)
+
+            var voteUrl = url + '/' + votecount;
+            console.log(voteUrl)
+
+             $.put(voteUrl, function(result) {
+                 console.log(voteUrl);
+                    result = result[0];
+           console.log('$.put update', result);
+        });  //end put
+
+        }
+    }; //end voteUpdate()
+
+    function dateCalc(created){
+
+            var today = new Date();
+            var createOn = new Date(created);
+            var diff = Math.floor((today - createOn));
+            var diffminutes = diff / (60 * 1000);
+                
+            if (diffminutes < 1440) {
+                 console.log('less then a day true')
+                 return true;
+             } 
+            console.log('diff', diff, 'diffday', diffday,
+                 'diffminutes', diffminutes);
+            console.log('today', today, 'created', createOn);
+    }  //  end dateCalc
+
+});  //end $.get(url, function(result)
+} // end votehandle
+   
+}); //end on votebutton click
+}); //send on ready function
--- server.js
+++ server.js
@@ -1,64 +1,50 @@
-/*
-Here is where you set up your server file.
-express middleware.*/
 
 var express = require('express');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 var exphbs = require('express-handlebars');
-var Submissions = require('./models')['Submissions'];
 
 var app = express();
 
-// creates all the tables in the models directories
+var port = process.env.PORT || 3306;
+
+var Submissions = require('./models')['Submissions'];
+
+var contestantVotes = require('./models')['contestantVotes'];
+
+// creates all tables in models directories
 
 global.db = require("./models");
-// var Submissions = require('./models')['Submissions'];
 
-// var sequelizeConnection = models.sequelize;
-// sequelizeConnection.query('SET FOREIGN_KEY_CHECKS = 0');
-
-
-// models.sequelize.sync();  //sync all tables
-
-//database setup
-// var Sequelize = require ('sequelize'), connection;
-// if (process.env.JAWSDB_URL) {
-//   connection = new Sequelize(process.env.JAWSDB_URL);
-// }else {
-//   connection = new Sequelize('coverTheseLyrics', 'root', 'password', {
-//     host: 'localhost',
-//     dialect: 'mysql',
-//     port: '3306'
-//   })
-// }
 
 app.use(express.static(process.cwd() + '/public')); 
 
 app.use(bodyParser.urlencoded({
-	extended: false
+  extended: false
 }));
 
 app.use(methodOverride('_method'));  
 
 app.engine('handlebars', exphbs({
-	defaultLayout: 'main'
+  defaultLayout: 'main'
 }));
 
 app.set('view engine', 'handlebars');
 
 //***********Routes to Handlebars TravisG**********//
 
+
+
 app.get('/', function(req, res) {
-	res.render('index');
+  res.render('index');
 });
 
 app.get('/lyrics', function(req, res) {
-	res.render('lyrics');
+  res.render('lyrics');
 });
 
 app.get('/submit-video', function(req, res) {
-	res.render('submit');  //get form for entry
+  res.render('submit');  //get form for entry
 });
 app.post('/submit-video', function(req,res){  //send form data to db
     var body = req.body;
@@ -77,31 +63,62 @@ app.post('/submit-video', function(req,res){  //send form data to db
   })
 });
 
-
-app.get('/vote', function(req, res){
-    Submissions.findAll({})
-      .then(function(result) {
+// initial vote page with submissions 
+app.get('/vote', function(req, res) {
+    Submissions.findAll({}).then(function(result) {
         console.log(result);
         return res.render('vote', {
             Submissions: result
         });
     });
-  });
+}); //end app.get 1
 
+//vote test/lookup for prev vote
+app.get('/vote/:ip/:entry_id', function(req, res) {
+            console.log('JW *** app.get db select ');
+            var ip = req.params.ip;
+            var entry_id = req.params.entry_id;
+            contestantVotes.findOne({
+                where: {
+                  ip: ip,
+                  entry_id: entry_id
+                }
+            }).then(function(result) {
+                return res.json(result);
+            });
+    //     .then(function(result) {
+  //console.log('here is your result:',result);
+    //     res.send(result)   
+    //});
 
-app.post('/vote/:ip?/:entryid?',function(req, res){
-   console.log('DATA CHECK JW',req.params.ip, req.params.entryid);
- var body = req.params;
+       });
+  
+app.post('/vote/:ip/:entry_id/:vote_counts',function(req, res){
+   console.log('NEW VOTE JW',req.params.ip, req.params.entry_id, req.params.vote_counts);
+
+   var body = req.params;
     db.contestantVotes.create({
-        ip: body.ip,
-       entryid: body.entryid,
-      }).then(function(data){
-        console.log('data',data);
-    //res.redirect('/submit-video/');
-    res.render('vote');
+       ip: body.ip,
+       entry_id: body.entry_id,
+       vote_counts:body.vote_counts
+      }).then(function(result){
+      return res.send(result);
 
   });
+});
 
+app.put('/vote/:ip/:entry_id/:vote_counts',function(req, res){
+   console.log('UPDATE VOTE JW',req.params.ip, req.params.entry_id, req.params.vote_counts);
+
+   var body = req.params;
+    db.contestantVotes.create({
+       ip: body.ip,
+       entry_id: body.entry_id,
+       vote_counts:body.vote_counts
+      }).then(function(result){
+      return res.send(result);
+
+  });
 });
 
 
@@ -115,7 +132,5 @@ var port = 3306;
 db.sequelize.sync().then(function(){
   app.listen(port, function(){
     console.log('connected to port ', port);
-  }); //JW moved best practice has the listening app last in the file
+  }); 
 });
-
-  
