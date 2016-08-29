65b91b5b19974576db1f9d1610858cf25c8b7db9
jenward <jenforce@aol.com>
2016-08-29 17:44:48 -0400

    refix previous fixes

--- public/assets/javascript/vote.js
+++ public/assets/javascript/vote.js
@@ -1,11 +1,14 @@
 $(document).ready(function() {
 $('.votebutton').on("click", function() {
-    console.log('clicked')
-
+    
     var entry_id = $(this).attr("id");
-    console.log($(this).attr("id"));
-
     var ip
+    var d = new Date();
+    var currentDate = d.toISOString().substring(0, 10);
+
+
+    console.log('entry_id',entry_id);
+    console.log('d',d,'currentDate',currentDate)
 
     getIP();
 
@@ -14,80 +17,87 @@ function getIP() {
         $.getJSON("https://api.ipify.org?format=jsonp&callback=?",
             function(json) {
                 ip = json.ip;
-                votehandle(ip);
+                voteHandler(ip);
             });
     });
 }; // end ip get
 
-function votehandle(ip) {
-     console.log(ip)
-     var url = '/vote/' + ip + '/' + entry_id;
-     console.log(url);
 
-     $.get(url, function(result) {
-           console.log('vote get result',result);
+function voteHandler(ip) {
+        console.log('start voteHandler function')
+        console.log('ip received', ip);
 
-         if (result === null) {
-            voteNew(url);
+        var d = new Date();
+        var today = d.toISOString();
+        var formatted_date = today.slice(0, 10);
+        //var url = '/vote/' + ip + '/' + entry_id + '/' + formatted_date;
+        var url = '/vote/' + ip + '/' + entry_id + '/' + formatted_date;
+        console.log('voteHandler: var today', today);
+        console.log('voteHandler: check for record var url', url);
+        //check if voted 
+        $.get(url, function(result) {
+            console.log('vote get result', result);
+            if (result === null) {
+                console.log('result = null call voteNew()');
+                voteNew(url);
+            } else {
+                console.log('else call voteUpdate()');
+                voteUpdate(url);
+            }
 
-         } else {
-            voteUpdate(url);
-        }
-             
-    function voteNew(url) {
-        console.log('true:vote_count === null')
-        var voteUrl = url + '/' + 1;
-        $.post(voteUrl, function(result) {
-            console.log(voteUrl);
-            result = result[0];
-            console.log('$.post new rec', result);
-        });  //end newVote
-    } //end newVote
+            function voteNew(url) {
+                    console.log('in new vote count');
+                    console.log('true:vote_count === null');
+                    var voteUrl = '/vote/' + ip + '/' + 1 + '/' + entry_id + '/' + formatted_date
 
+                    console.log('/' + 'post vote url',voteUrl  + '/');
+                    $.post(voteUrl, function(result) {
+                        
+                        result = result[0];
+                        console.log('$.post new rec result', result);
+                    }); //end newVote
+                } //end newVote
 
-    function voteUpdate(){
-        console.log('in voteUpdate', result);
-        var id = result.id
-        var createdAt = result.createdAt;
-        var dateCheck = dateCalc(createdAt);
-        var votes = result.vote_counts;
-            url = '/vote/' + id + '/' + votes;
+            function voteUpdate() {
+                    console.log('in voteUpdate', result);
+                    var id = result.id
+                    var createdAt = result.createdAt;
+                    var dateCheck = dateCalc(createdAt);
+                    var votes = result.vote_counts;
+                    url = '/vote/' + id + '/' + votes;
+                    console.log('url', url);
+                    console.log(dateCheck);
+                    if (votes < 2 && dateCheck === true) {
+                        var votecount = votes + 1;
+                        url = '/vote/' + id + '/' + votecount;
+                        console.log('in vote logic');
+                        console.log('vote', votes, 'votecount',
+                            votecount);
+                        $.post(url, function(result) {
+                            console.log(url);
+                            result = result[0];
+                            console.log('$.post update', result);
+                        }); //end put
+                    } //end true 
+                } //end voteUpdate()
 
-        console.log('url',url);
+            function dateCalc(created) {
+                    var todaycompare = new Date();
+                    var createOn = new Date(created);
+                    var diff = Math.floor((todaycompare - createOn));
+                    var diffminutes = diff / (60 * 1000);
+                    if (diffminutes < 1440) {
+                        console.log('less then a day true');
+                        return true;
+                    }
+                    console.log('diff', diff, 'diffminutes',
+                        diffminutes);
+                    console.log('todaycompare', todaycompare, 'created',
+                        createOn);
+                } //  end dateCalc
+        }); //end $.get(url, function(result)
+    } // end votehandle
 
-        if (votes < 2 && dateCheck == true) {
-            var votecount = votes + 1;
-             url = '/vote/' + id + '/' + votecount;
-
-            console.log('in vote logic');
-            console.log('vote',votes,'votecount',votecount);
-
-             $.post(url, function(result) {
-                 console.log(url);
-                 result = result[0];
-                 console.log('$.post update', result);
-            });  //end put
-
-        }
-    }; //end voteUpdate()
-
-    function dateCalc(created){
-
-            var today = new Date();
-            var createOn = new Date(created);
-            var diff = Math.floor((today - createOn));
-            var diffminutes = diff / (60 * 1000);
-                
-            if (diffminutes < 1440) {
-                 console.log('less then a day true')
-                 return true;
-             } 
-            console.log('diff', diff, 'diffday', diffday,
-                 'diffminutes', diffminutes);
-            console.log('today', today, 'created', createOn);
-    }  //  end dateCalc
-});  //end $.get(url, function(result)
-} // end votehandle
    
 }); //end on votebutton click
 }); //send on ready function
