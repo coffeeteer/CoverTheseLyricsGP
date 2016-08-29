4c6ad76a89b4dac12e7c1975a0e6c381c8d0082d
Seth Keel <seth.keel@gmail.com>
2016-08-28 22:13:37 -0400

    second commit

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var exphbs = require('express-handlebars');
 var app = express();
 
 console.log('process.env.PORT', process.env.PORT);
-var port = process.env.PORT || 3307;
+var port = process.env.PORT || 3306;
 
 var Submissions = require('./models')['Submissions'];
 var contestantVotes = require('./models')['contestantVotes'];
